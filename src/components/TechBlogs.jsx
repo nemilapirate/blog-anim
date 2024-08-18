718a23280c05ac7f1c14f2ba0083a19d83391e30
StayssTheRED <71025281+nemilapirate@users.noreply.github.com>
2024-08-18 15:55:32 +0200

    Update TechBlogs.jsx
    mise à jour du composant des article de la section tech

--- src/components/TechBlogs.jsx
+++ src/components/TechBlogs.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState } from "react";
 import { blogs } from "../data/blogs";
 import Blog from "../ui/Blog";
 import AsideBlogs from "../ui/AsideBlogs";
@@ -15,12 +16,19 @@ const TechBlogsBody = styled.div`
 const TechBlogsBodyBig = styled.div`
   flex: 2;
 `;
+
 const TechBlogsBodyRest = styled.aside`
   flex: 1;
 `;
 
 export default function TechBlogs() {
-  const [firstTechBlog, ...restBlogs] = blogs;
+  // Utilisation du hook useState pour gérer l'état de l'article principal sélectionné
+  const [selectedBlog, setSelectedBlog] = useState(blogs[0]);
+
+  // Cette fonction met à jour l'article principal lorsqu'un article latéral est cliqué
+  const handleBlogClick = (blog) => {
+    setSelectedBlog(blog);
+  };
 
   return (
     <StyledTechBlogs>
@@ -35,11 +43,17 @@ export default function TechBlogs() {
       />
       <TechBlogsBody>
         <TechBlogsBodyBig>
-          <Blog blog={firstTechBlog} fixedSizeImg={true} />
+          {/* L'article principal est affiché ici */}
+          <Blog blog={selectedBlog} fixedSizeImg={true} />
         </TechBlogsBodyBig>
         <TechBlogsBodyRest>
-          {restBlogs.map((blog) => (
-            <AsideBlogs blog={blog} key={blog.id} />
+          {/* Les articles latéraux sont affichés ici */}
+          {blogs.map((blog) => (
+            <AsideBlogs 
+              blog={blog} 
+              key={blog.id} 
+              onClick={() => handleBlogClick(blog)} // Gère le clic sur un article
+            />
           ))}
         </TechBlogsBodyRest>
       </TechBlogsBody>
